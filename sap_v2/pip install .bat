5ce90e375bc331a92c372df032c13b751e197321
ankit-git00 <125370874+ankit-git00@users.noreply.github.com>
2023-03-07 13:11:22 +0530

    Add files via upload

---
+++ sap_v2/pip install .bat
@@ -0,0 +1,8 @@
+pip install selenium
+pip install pyinstalller
+python write_input.py
+pyinstaller --onefile main.py
+move new.txt dist
+move run_this_file.bat dist
+cd dist
+start run_this_file.bat
